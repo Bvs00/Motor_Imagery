ff73182ca51a076e68e9c05c2f63c91c247100b2
Bvs00 <s.bove21@studenti.unisa.it>
2025-08-04 18:09:01 +0000

    I update segmentation_reconstruction function to do faster

--- data_augmentation.py
+++ data_augmentation.py
@@ -45,22 +45,28 @@ def segmentation_reconstruction(data, labels, num_segments=8, num_augmentations=
     full_data = data.clone()
     full_labels = labels.clone()
     type_labels = torch.unique(labels)
-    length_segment=data.shape[3]//num_segments
+    length_segment=time//num_segments
+    
     for label in type_labels:
         idx_labels=torch.where(labels == label)[0]
-        data_for_label = data[idx_labels,:,:,:]
-        num_samples_for_classes = data_for_label.shape[0]*num_augmentations
+        data_for_label = data[idx_labels]
+        n_samples=data_for_label.size(0)
+        num_samples_for_classes = n_samples*num_augmentations
+        
         label_tmp = labels[idx_labels][0]
-        new_data = torch.zeros((num_samples_for_classes, conv, ch, time), device=data.device)
-        for i in range(num_samples_for_classes):
-            for seg_idx in range(num_segments):
-                # Selezioniamo casualmente un campione dal batch originale
-                random_sample = random.randint(0, data_for_label.shape[0] - 1)
-                # Prendiamo il segmento corretto e lo assembliamo nel nuovo campione
-                start = seg_idx * length_segment
-                end = (seg_idx + 1) * length_segment
-                # Assembliamo mantenendo la struttura [batch, 1, 3, 1000]
-                new_data[i, :, :, start:end] = data_for_label[random_sample, :, :, start:end]
+        new_data = torch.empty((num_samples_for_classes, conv, ch, time), device=data.device)
+        
+        # Creiamo un vettore di dimensione num_samples_for_classes x num_segments con all'interno dei valori 
+        # randomici presi dall'insieme dei numeri che vanno da [0-n_samples]
+        random_sample = torch.randint(0, n_samples, (num_samples_for_classes, num_segments), device=data.device)
+        
+        for seg_idx in range(num_segments):
+
+            start = seg_idx * length_segment
+            end = (seg_idx + 1) * length_segment
+            #creazione del nuvo vettore new_data
+            new_data[:, :, :, start:end] = data_for_label[random_sample[:, seg_idx], :, :, start:end]
+        
         full_data = torch.cat([full_data, new_data], dim=0)
         full_labels = torch.cat([full_labels, label_tmp.repeat(num_samples_for_classes)], dim=0)
     idx_shuffled = torch.randperm(full_data.size(0))
