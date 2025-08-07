a18602743e5487eb0c8c67fe8730dd1eee73740c
Bvs00 <s.bove21@studenti.unisa.it>
2025-08-07 17:54:53 +0200

    Remove trash file, Previous LOSO Validation wrong, on cluster

--- main_script_cluster.sh
+++ main_script_cluster.sh
@@ -5,9 +5,10 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --gpus-per-task=1
 #SBATCH --time=07:00:00
-#SBATCH --nodelist=gnode05
-#SBATCH --job-name=patch_cross
-#SBATCH --output=patch_cross.log
+#SBATCH --nodelist=gnode09
+#SBATCH --job-name=ctnet_final
+#SBATCH --output=ctnet_final.log
+#SBATCH --dependency=80869
 
 export TORCH_DEVICE=cuda
 export PYTHON=/home/bvosmn000/.conda/envs/ICareMeEnv/bin/python
@@ -28,15 +29,15 @@ echo "$PARADIGM"
 echo "$TORCH_DEVICE"
 
 if [ "$PRIME" == "1" ]; then
-  primes=(42 71 101 113 127 131 139 149 157 163 173 181 322 521)
+  primes=(71 101 113 127 131 139 149 157 163 173 181 322 521)
 elif [ "$PRIME" == "2" ]; then
   primes=(402 701 1001 1013 1207 1031 1339 1449 1527 1613 1743 1841 3222 5421)
 elif [ "$PRIME" == "3" ]; then
   primes=(42 71 101 113 127 131 139 149 157 163 173 181 322 521 402 701 1001 1013 1207 1031 1339 1449 1527 1613 1743 1841 3222 5421)
 elif [ "$PRIME" == "4" ]; then
-  primes=(131 139 157 163 173 181 322 521)
+  primes=(322 521)
 elif [ "$PRIME" == "5" ]; then
-  primes=(149)
+  primes=(131)
 fi
 
 
@@ -51,7 +52,7 @@ paradigm="$PARADIGM"
 for seed in "${primes[@]}"; do
   echo "Train seed: $seed"
   $PYTHON -u train_motor_imagery.py --seed "$seed" --name_model "$network" --saved_path "$saved_path" --lr 0.001 \
-          --augmentation "$aug" --num_workers 5 --normalization "$normalization" --paradigm "$paradigm" \
+          --augmentation "$aug" --num_workers 32 --normalization "$normalization" --paradigm "$paradigm" \
           --train_set "/mnt/beegfs/sbove/2B/train_2b_$bandpass.npz" --device "$TORCH_DEVICE"\
           --patience 300 --batch_size 72
   $PYTHON -u test_motor_imagery.py --name_model "$network" --saved_path "$saved_path" --paradigm "$paradigm" \
