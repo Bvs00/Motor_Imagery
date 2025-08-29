fabf6cdedba822fa1e30f08e4253541fc3002218
Bvs00 <s.bove21@studenti.unisa.it>
2025-08-29 15:21:28 +0200

    Fix bug in test_motor_imagery, I forget to change the loss in test if my model is MSVT

--- test_motor_imagery.py
+++ test_motor_imagery.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from utils import create_tensors, find_minum_loss, validate, plot_confusion_matrix, \
-    load_normalizations, available_paradigm, available_network, network_factory_methods
+    load_normalizations, available_paradigm, available_network, network_factory_methods, JointCrossEntoryLoss
 import argparse
 from torch.utils.data import TensorDataset, DataLoader
 import json
@@ -57,7 +57,10 @@ if __name__ == '__main__':
         model.to(args.device)
         model.load_state_dict(torch.load(f'{saved_path}/{args.name_model}_seed{args.seed}_best_model_fold{best_fold}.pth'))
 
-        criterion = nn.CrossEntropyLoss()
+        if args.name_model=='MSVTNet':
+            criterion = JointCrossEntoryLoss()
+        else:
+            criterion = nn.CrossEntropyLoss()
 
         avg_loss, f1, confusion_matrix, accuracy, balanced_accuracy = validate(model, test_loader, criterion, args.device)
 
