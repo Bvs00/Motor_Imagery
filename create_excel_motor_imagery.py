8b1467785cb2629bbb3b1404e435e8a156895941
Bvs00 <s.bove21@studenti.unisa.it>
2026-05-25 10:23:15 +0200

    Create_Excel has not fixed 9 as number of subjects

--- create_excel_motor_imagery.py
+++ create_excel_motor_imagery.py
@@ -13,12 +13,21 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     path = args.path
-    f1_score_seeds = {'Patient1':[], 'Patient2':[], 'Patient3':[], 'Patient4':[], 'Patient5':[], 'Patient6':[],
-                                    'Patient7':[], 'Patient8':[], 'Patient9':[], 'Average':[]}
-    dataframe = pd.DataFrame(columns=['Seed', 'Patient1', 'Patient2', 'Patient3', 'Patient4', 'Patient5', 'Patient6',
-                                    'Patient7', 'Patient8', 'Patient9', 'Average'])
-    dataframe_balanced = pd.DataFrame(columns=['Seed', 'Patient1', 'Patient2', 'Patient3', 'Patient4', 'Patient5', 'Patient6',
-                                    'Patient7', 'Patient8', 'Patient9', 'Average'])
+    if 'OpenBMI' in path:
+        num_patient = 54
+    else:
+        num_patient = 9
+    f1_score_seeds = {}
+    for i in range(num_patient):
+        f1_score_seeds[f'Patient{i+1}'] = []
+    f1_score_seeds['Average'] = []
+    
+    columns = ['Seed'] + [f'Patient{i+1}' for i in range(num_patient)] + ['Average']
+    # f1_score_seeds = {'Patient1':[], 'Patient2':[], 'Patient3':[], 'Patient4':[], 'Patient5':[], 'Patient6':[],
+    #                                 'Patient7':[], 'Patient8':[], 'Patient9':[], 'Average':[]}
+    dataframe = pd.DataFrame(columns=columns)
+    dataframe_balanced = pd.DataFrame(columns=columns)
+    
     list_seeds = [42, 71, 101, 113, 127, 131, 139, 149, 157, 163, 173, 181, 322, 521, 
                   402, 701, 1001, 1013, 1207, 1031, 1339, 1449, 1527, 1613, 1743, 
                   1841, 3222, 5421] if args.full_seeds else [42, 71, 101, 113, 127, 131, 
@@ -30,17 +39,17 @@ if __name__ == "__main__":
 
         with open(file_path, 'r') as f:
             data = json.load(f)
-            accuracies = [round(data[i]['Accuracy'], 3) for i in range(9)]
-            balanced_accuracies = [round(data[i]['Balanced Accuracy'], 3) for i in range(9)]
-            
-            for i in range(9):
+            accuracies = [round(data[i]['Accuracy'], 3) for i in range(num_patient)]
+            balanced_accuracies = [round(data[i]['Balanced Accuracy'], 3)*100 for i in range(num_patient)]
+
+            for i in range(num_patient):
                 key = f'Patient{i+1}'
                 f1_val_arr = [round(x, 3) for x in data[i]['F1 Score']]
                 if f1_score_seeds[key]:
                     f1_score_seeds[key] = [a+b for a,b in zip(f1_score_seeds[key], f1_val_arr)]
                 else:
                     f1_score_seeds[key] = f1_val_arr
-            f1_val_arr = [round(x, 3) for x in data[9]['Average']['F1 Score']]
+            f1_val_arr = [round(x, 3) for x in data[num_patient]['Average']['F1 Score']]
             if f1_score_seeds['Average']:
                 f1_score_seeds['Average'] = [a+b for a,b in zip(f1_score_seeds['Average'], f1_val_arr)]
             else:
@@ -53,7 +62,7 @@ if __name__ == "__main__":
         dataframe_balanced['Seed'] = dataframe_balanced['Seed'].astype('int32')
     
     tmp_acc_list, tmp_bal_acc_list = [], []
-    for i in range(9):
+    for i in range(num_patient):
         key = f'Patient{i+1}'
         f1_score_seeds[key] = [round(x / len(list_seeds), 3) for x in f1_score_seeds[key]]
         tmp_acc_list.append(round(dataframe[key].mean(), 3))
